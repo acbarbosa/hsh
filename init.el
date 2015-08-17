759561790ef7d01901adee0698dd1a430c98000b
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2015-08-17 14:46:38 -0300

    Configured marmalade and melpa as package repositories

---
+++ init.el
@@ -0,0 +1,7 @@
+;; Setting up Marmalade repo
+(require 'package)
+(add-to-list 'package-archives
+    '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+    '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
