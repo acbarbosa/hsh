b422ba285c5e4487988121f4ec9374e0ed94826d
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2015-08-17 14:51:03 -0300

    Configured color-theme

--- init.el
+++ init.el
@@ -5,3 +5,11 @@
 (add-to-list 'package-archives
     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
+
+
+;; Configuring color-theme
+(add-to-list 'load-path "~/.emacs.d/packages/color-theme")
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn (color-theme-initialize)
+          (color-theme-goldenrod)))
