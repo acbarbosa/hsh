769436752ff26098c5da7fb10ccd8b0804e82dd0
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2015-12-11 11:54:49 -0200

    Changed configuration of org-agenda-files

--- org-mode.el
+++ org-mode.el
@@ -1,6 +1,7 @@
 (require 'org)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(setq org-agenda-files (list "~/Documents/org-agenda/tw.org"
+
+(setq org-agenda-files (list "~/Documents/org-agenda/tw"
                              "~/Documents/org-agenda/personal.org"))
 (setq org-log-done t)
