bb1eb736a709af6816ac67ed9f284c84ae2624a8
Alexandre C. Barbosa <Alexandre_Barbosa@external.mckinsey.com>
2018-03-01 11:21:30 -0300

    Installed markdown-mode

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (projectile magit flycheck editorconfig color-theme))))
+    (markdown-mode projectile magit flycheck editorconfig color-theme))))
 (unless package-archive-contents
   (package-refresh-contents))
 (package-install-selected-packages)
