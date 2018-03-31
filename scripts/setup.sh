640633b7d93282653f00a4086d6495c7b4fde2ae
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2018-03-30 21:44:16 -0300

    Installs google-chrome

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,3 +10,9 @@ command -V /usr/local/bin/emacs > /dev/null 2>&1 || {
     brew install emacs
 }
 
+
+if [ ! -e "/Applications/Google Chrome.app" ]; then
+    brew cask install google-chrome
+fi
+
+
