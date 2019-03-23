69af502807814b4e0db2ac397795fe53264c3e16
Alexandre Corrêa Barbosa <acbarbosa@Alexandres-MacBook-Pro.local>
2019-03-23 11:04:02 -0300

    Fixes issue that prevented to execute setup.sh from anywhere

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 command -V brew > /dev/null 2>&1 || {
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
 
-brew bundle
-brew bundle --file=Brewfile.developer
+brew bundle --file="${SCRIPT_DIR}/Brewfile"
+brew bundle --file="${SCRIPT_DIR}/Brewfile.developer"
