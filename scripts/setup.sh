bc549fbc9fd6e64141e4919a9349e7e45e4edce0
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2019-05-17 20:00:22 -0300

    Installs leiningen (clojure configuration)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,5 +6,8 @@ command -V brew > /dev/null 2>&1 || {
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
 
-brew bundle --file="${SCRIPT_DIR}/Brewfile"
-brew bundle --file="${SCRIPT_DIR}/Brewfile.developer"
+brew update && brew upgrade
+
+brew bundle --file="${SCRIPT_DIR}/Brewfile" -v
+brew bundle --file="${SCRIPT_DIR}/Brewfile.developer" -v
+brew bundle --file="${SCRIPT_DIR}/Brewfile.clojure" -v
