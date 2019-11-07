c0363355fc416e5b42c95c3a6a80052fc419fb35
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2019-11-06 23:17:50 -0300

    Creates /usr/local/Frameworks required by homebrew

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,11 @@ command -V brew > /dev/null 2>&1 || {
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
 
+if [ ! -d /usr/local/Frameworks ]; then
+    sudo mkdir -p /usr/local/Frameworks
+    sudo chown -R "$(whoami)" /usr/local/Frameworks
+fi
+
 brew update && brew upgrade
 
 brew bundle --file="${SCRIPT_DIR}/Brewfile" -v
