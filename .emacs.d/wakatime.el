aa508d9f01be8c838d36505d2ca7ef99ecf90d70
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2019-11-09 22:07:49 -0300

    Removes secret from repo

--- .emacs.d/wakatime.el
+++ .emacs.d/wakatime.el
@@ -1,3 +1,3 @@
-(setq wakatime-api-key "776ad9c0-d26b-4818-bfa3-a29ad83df597")
+(setq wakatime-api-key (getenv "WAKATIME_API_TOKEN"))
 (setq wakatime-cli-path "/usr/local/bin/wakatime")
 (global-wakatime-mode)
