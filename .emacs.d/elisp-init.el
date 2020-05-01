b86e11336635bde645da85ff6780e97c119858d9
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2020-05-01 19:02:47 -0300

    Migrates elisp configuration to use-package

--- .emacs.d/elisp-init.el
+++ .emacs.d/elisp-init.el
@@ -1,10 +1,10 @@
-(unless (package-installed-p 'rainbow-delimiters)
-  (package-install 'rainbow-delimiters))
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (emacs-lisp-mode-hook . rainbow-delimiters-mode))
 
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             (show-paren-mode)
             (setq show-paren-style 'expression)
-            (rainbow-delimiters-mode)
-            (eldoc-mode)))
-(add-hook 'emacs-lisp-mode-hook 'linum-mode)
+            (eldoc-mode)
+            (linum-mode)))
