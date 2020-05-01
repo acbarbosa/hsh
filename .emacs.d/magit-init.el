614bdd271f2635277f1d3fd386096ba665119f30
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2020-05-01 15:47:11 -0300

    Migrates magit configuration to use-package

--- .emacs.d/magit-init.el
+++ .emacs.d/magit-init.el
@@ -1,9 +1,3 @@
-(unless (package-installed-p 'magit)
-  (package-install 'magit))
-
-;; Turning on global-magit-file-buffer-mode
-;;(global-magit-file-buffer-mode)
-
-;; Shortcuts
-(global-set-key (kbd "C-x g") 'magit-status)
-
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . 'magit-status))
