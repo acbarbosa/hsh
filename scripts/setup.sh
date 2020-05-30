5af56427d2e20cc90d03d6118f5626e2ee450d44
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2020-05-30 15:26:08 -0300

    Extracts brew setup into a function

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,19 +2,23 @@
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-command -V brew > /dev/null 2>&1 || {
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+setup_brew() {
+    command -V brew > /dev/null 2>&1 || {
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    }
+
+    if [ ! -d /usr/local/Frameworks ]; then
+        sudo mkdir -p /usr/local/Frameworks
+        sudo chown -R "$(whoami)" /usr/local/Frameworks
+    fi
+
+    brew update && brew upgrade
+
+    brew bundle --file="${SCRIPT_DIR}/Brewfile" -v
+    brew bundle --file="${SCRIPT_DIR}/Brewfile.developer" -v
+    brew bundle --file="${SCRIPT_DIR}/Brewfile.clojure" -v
 }
 
-if [ ! -d /usr/local/Frameworks ]; then
-    sudo mkdir -p /usr/local/Frameworks
-    sudo chown -R "$(whoami)" /usr/local/Frameworks
-fi
-
-brew update && brew upgrade
-
-brew bundle --file="${SCRIPT_DIR}/Brewfile" -v
-brew bundle --file="${SCRIPT_DIR}/Brewfile.developer" -v
-brew bundle --file="${SCRIPT_DIR}/Brewfile.clojure" -v
+setup_brew
 
 [ ! -d "${HOME}/.nvm" ] && mkdir "${HOME}/.nvm"
