9994d7a25d2c56af90e3417e09f99cb33c1598bf
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2021-03-23 17:46:53 -0300

    Configures emacs as typescript ide with lsp

--- .emacs.d/config/typescript.el
+++ .emacs.d/config/typescript.el
@@ -1,16 +1,9 @@
-(use-package company
-  :ensure t)
-
-
-(use-package tide
+(use-package typescript-mode
   :ensure t
-  :after  (typescript-mode company flycheck)
-  :hook   ((typescript-mode-hook . tide-setup)
-           (typescript-mode-hook . tide-hl-identifier-mode)
-           (typescript-mode-hook . linum-mode)
-           (before-save-hook . tide-format-before-save)))
+  :hook ((typescript-mode-hook . lsp)
+         (typescript-mode-hook . linum-mode)))
 
 (use-package web-mode
   :ensure t
-  :mode   ("\\.tsx\\'" . web-mode)
-  :hook   (web-mode-hook . typescript-mode))
+  :hook (web-mode-hook . typescript-mode)
+  :mode ("\\.tsx\\'" . web-mode))
