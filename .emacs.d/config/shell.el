f0c367802164b3eef654070c83d85c3d42958463
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2022-05-21 20:08:33 -0300

    Replaces flycheck/shellcheck with lsp/bash-language-server for bash file editing

--- .emacs.d/config/shell.el
+++ .emacs.d/config/shell.el
@@ -1,11 +1,3 @@
-(use-package flycheck
-  :hook (sh-mode-hook . flycheck-mode)
-  :init
-  ;; Configure shellcheck to run on sh files
-  (flycheck-define-checker sh-shellcheck
-    "A shell script syntax using Shellcheck"
-    :command ("shellcheck" "-f" "checkstyle"
-              "-s" (eval (symbol-name sh-shell))
-              source)
-    :modes sh-mode
-    :error-parser flycheck-parse-checkstyle))
+(use-package sh-script
+  :hook ((sh-mode-hook . lsp)
+         (sh-mode-hook . linum-mode)))
