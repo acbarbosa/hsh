2eb9d8d834d6b6fe9581e1aebeea39dd47285039
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2023-05-01 21:24:53 -0300

    Adds lsp-ui and lsp-treemacs to lsp configuration

--- .emacs.d/config/lsp.el
+++ .emacs.d/config/lsp.el
@@ -4,9 +4,16 @@
   :config
   (which-key-mode))
   
+(use-package yasnippet)
+
 (use-package lsp-mode
   :after (company which-key)
   :init
   (setq lsp-keymap-prefix "C-c l")
-  :hook (lsp-mode . lsp-enable-which-key-integration)
+  :hook ((lsp-mode-hook . lsp-enable-which-key-integration)
+         (lsp-mode-hook . yas-minor-mode))
   :commands lsp)
+
+(use-package lsp-ui)
+
+(use-package lsp-treemacs)
