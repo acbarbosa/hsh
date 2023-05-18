2db006cbee3d62d5d3be0fc98a7f81fe2bf89277
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2023-05-17 21:18:48 -0300

    Configures emacs css-mode with lsp

--- .emacs.d/config/web-development.el
+++ .emacs.d/config/web-development.el
@@ -1,9 +1,18 @@
-(use-package web-mode
-  :mode (("\\.html\\'" . web-mode)
-         ("\\.css\\'" . web-mode)
-         ("\\.scss\\'" . web-mode)
-         ("\\.tsx\\'" . web-mode))
-  :hook (web-mode-hook . lsp)
+(use-package css-mode
+  :straight (:type built-in)
+  :hook ((css-mode-hook . lsp)
+         (css-mode-hook . hs-minor-mode))
   :commands lsp)
 
 (add-to-list 'lsp-language-id-configuration '("\\.scss\\'" . "scss"))
+
+
+(use-package web-mode
+  :mode ("\\.html\\'" "\\.tsx\\'")
+  :hook ((web-mode-hook . lsp)
+         (web-mode-hook . linum-mode))
+  :config
+  (setq web-mode-auto-close-style 2)
+  :commands lsp)
+
+
