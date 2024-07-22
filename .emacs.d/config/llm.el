c07690a02945c033ec59acb48dc6825c5449c5e7
Alexandre Corrêa Barbosa <alexandre.correa@gmail.com>
2024-07-22 20:03:40 -0300

    Adds spanish-helper prompt to emacs' gptel config

--- .emacs.d/config/llm.el
+++ .emacs.d/config/llm.el
@@ -1,4 +1,7 @@
 (use-package gptel
   :ensure t
-  :config (setq gptel-api-key (auth-source-pick-first-password :host "api.openai.com")
-                gptel-model "gpt-4o"))
+  :config
+  (setq gptel-api-key (auth-source-pick-first-password :host "api.openai.com")
+        gptel-model "gpt-4o")
+  (add-to-list 'gptel-directives '(spanish-helper . "You're a spanish teacher that will help me to write better texts in Spanish. Please correct my grammar and ortographic mistakes, suggests better ways to write the sentences, focusing on clarity and also to use more common words and expressions. Please, highlight the changes and please explain to me each one of the suggestions.")))
+
